c3b168be3aa3d1780d8558b850be1b4c5d31f82a
Matheus Nobre <matheusnobre56@gmail.com>
2022-01-09 19:59:15 -0300

    requiring guzzlehttp dependencies

---
+++ app/Traits/ConsumesExternalService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Traits;
+
+use GuzzleHttp\Client;
+
+trait ConsumesExternalService
+{
+
+    /**
+     * Send a request to any service;
+     *
+     * @param string $method
+     * @param string $requestUrl
+     * @param array $formParams
+     * @param array $headers
+     * @return string
+     */
+    public function performRequest(
+        string $method,
+        string $requestUrl,
+        array $formParams = [],
+        array $headers = []
+    ): string
+    {
+        $client = new Client([
+            'base_uri' => $this->baseUri,
+        ]);
+
+        $response = $client->request(
+            $method,
+            $requestUrl,
+            [
+                'form_params' => $formParams,
+                'headers' => $headers,
+            ]
+        );
+
+        return $response->getBody()->getContents();
+    }
+}
